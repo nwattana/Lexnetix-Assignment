e02d02c5286d0e1a91344065b1f4ee9cce159e83
junk5 <junk5@gmail.com>
2023-01-24 12:33:14 +0000

    .

--- LexneticSchool/lexscool/tests.py
+++ LexneticSchool/lexscool/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+import unittest
+from django.test import Client
+from .schemas import *
+from .api import *
+from .models import *
 
-# Create your tests here.
+SCHOOL_IN_1 = SchoolBase(
+    name = "school 1",
+    email = "school 1",
+    address = "school 1",
+    tel = "school 1",
+)
+SCHOOL_IN_2 = SchoolBase(
+    name = "school 2",
+    email = "school 2",
+    address = "school 2",
+    tel = "school 2",
+)
+SCHOOL_IN_3 = SchoolBase(
+    name = "school 3",
+    email = "school 3",
+    address = "school 3",
+    tel = "school 3",
+)
+
+class BasicGet(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+        school1 = school_create(None, SCHOOL_IN_1)
+        school2 = school_create(None, SCHOOL_IN_2)
+        school3 = school_create(None, SCHOOL_IN_3)
+        
+    def test_get_school_1(self):
+        result = (200, schools.objects.get(id=1))
+        response = school_get_by_id(None, school_id = 1)
+        self.assertEqual(result, response)
+        
+    def test_get_school_2(self):
+        result = (200, schools.objects.get(id=2))
+        response = school_get_by_id(None, school_id = 2)
+        self.assertEqual(result, response)
+
+    def test_get_school_3(self):
+        result = (200, schools.objects.get(id=3))
+        response = school_get_by_id(None, school_id = 3)
+        self.assertEqual(result, response)
+        
+        
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/api/school')
+        self.assertEqual(response.status_code, 200)
